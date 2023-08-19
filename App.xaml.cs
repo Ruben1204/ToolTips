8113afaabea2e4e9bea80ed2e2a89fd23174caf8
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-08-19 16:23:17 -0500

    FirstCommit
    Add UI with TooltipsPropertys

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RecursosMauiNet7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.ToolTips();
+        }
+    }
+}
---
+++ Views/ToolTips.xaml.cs
@@ -0,0 +1,9 @@
+namespace RecursosMauiNet7.Views;
+
+public partial class ToolTips : ContentPage
+{
+	public ToolTips()
+	{
+		InitializeComponent();
+	}
+}
